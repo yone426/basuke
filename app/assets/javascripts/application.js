712861b18fa0a57c93fed3a39b37654b5f59dfd5
yone426 <yone14671467@yahoo.co.jp>
2020-03-15 02:04:55 +0900

    header_2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -94,3 +94,66 @@ $(function() {
     });
 });
 
+var navigationOpenFlag = false;
+var navButtonFlag = true;
+var focusFlag = false;
+ 
+//ハンバーガーメニュー
+    $(function(){
+ 
+      $(document).on('click','.el_humburger',function(){
+        if(navButtonFlag){
+          spNavInOut.switch();
+          //一時的にボタンを押せなくする
+          setTimeout(function(){
+            navButtonFlag = true;
+          },200);
+          navButtonFlag = false;
+        }
+      });
+      $(document).on('click touchend', function(event) {
+        if (!$(event.target).closest('.bl_header,.el_humburger').length && $('body').hasClass('js_humburgerOpen') && focusFlag) {
+          focusFlag = false;
+          //scrollBlocker(false);
+          spNavInOut.switch();
+        }
+      });
+    });
+ 
+//ナビ開く処理
+function spNavIn(){
+  $('body').removeClass('js_humburgerClose');
+  $('body').addClass('js_humburgerOpen');
+  setTimeout(function(){
+    focusFlag = true;
+  },200);
+  setTimeout(function(){
+    navigationOpenFlag = true;
+  },200);
+}
+ 
+//ナビ閉じる処理
+function spNavOut(){
+  $('body').removeClass('js_humburgerOpen');
+  $('body').addClass('js_humburgerClose');
+  setTimeout(function(){
+    $(".uq_spNavi").removeClass("js_appear");
+    focusFlag = false;
+  },200);
+  navigationOpenFlag = false;
+}
+ 
+//ナビ開閉コントロール
+var spNavInOut = {
+  switch:function(){
+    if($('body.spNavFreez').length){
+      return false;
+    }
+    if($('body').hasClass('js_humburgerOpen')){
+     spNavOut();
+    } else {
+     spNavIn();
+    }
+  }
+};
+
